3e258fe31bff14480f53f5889448c059b0a8f803
Sibghat776 <ullahsibghat786@gmail.com>
2024-12-22 17:08:42 +0500

    Creating Picture SlideBar

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ function previousHandler() {
 function nextHandler() {
     console.log("Me chal Raha hun nextHandler()")
     console.log(mainDiv)
-    mainDiv.style.backgroundImage = `url("./Images/Background Image three.jpg")`
-    count++
+    mainDiv.style.backgroundImage = `url("./Images/Background Image two.jpg")`
+    for (i = 0; count < 1 ; i++) {
+        mainDiv.style.backgroundImage = `url("./Images/Background Image three.jpg")`
+        console.log(count)
+        mainDiv.style.backgroundImage = `url("./Images/Background Image four.jpg")`
+        count++
+        break
+    }
 }
